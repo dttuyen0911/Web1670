b2bb174a5a9568d42aa6e0f3dd5047c518d3bdf3
dttuyen0911 <107165518+dttuyen0911@users.noreply.github.com>
2023-02-22 20:02:44 +0700

    2202(3)

---
+++ Data/Migrations/20230222130208_add3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GC02Identity.Data.Migrations
+{
+    public partial class add3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
