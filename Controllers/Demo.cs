652eb0c8b431ae95f7f6ca702888b1da036771aa
dttuyen0911 <107165518+dttuyen0911@users.noreply.github.com>
2023-02-22 18:53:40 +0700

    Add project files.

---
+++ Controllers/Demo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GC02Identity.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class Demo : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
