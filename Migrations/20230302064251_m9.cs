6d041e4de79df5a404b2fc20b03e07a256624f1c
dttuyen0911 <107165518+dttuyen0911@users.noreply.github.com>
2023-03-02 13:44:34 +0700

    0203(1)

---
+++ Migrations/20230302064251_m9.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web1670.Migrations
+{
+    public partial class m9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
