10b939d826f770cdd0e20c5c5453f5e7b6963a82
Tan Huynh <104268347+tan00444@users.noreply.github.com>
2023-03-03 11:17:18 +0700

    Up all

---
+++ Controllers/AppRolesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Web1670.Controllers
+{
+    public class AppRolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AppRolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles;
+            return View();
+        }
+    }
+}
