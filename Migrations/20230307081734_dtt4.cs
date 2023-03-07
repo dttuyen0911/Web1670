d0fb19b52c4c6d3f69c472e9d9f48f06061cbab7
dttuyen0911 <107165518+dttuyen0911@users.noreply.github.com>
2023-03-07 15:51:49 +0700

    0703(06)

---
+++ Migrations/20230307081734_dtt4.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web1670.Migrations
+{
+    public partial class dtt4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cus_id",
+                table: "carts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cus_id",
+                table: "carts");
+        }
+    }
+}
